e9f38d153c65a5623c729563ee4f28d85da56cbe
Daniel Conejo <wonno23@gmail.com>
2024-05-17 13:44:59 -0500

    Tercer Commit
    Primeras dos tablas

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.*;
 import com.itextpdf.text.pdf.PdfWriter;
 
+
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -13,33 +14,42 @@ import static com.itextpdf.text.FontFactory.getFont;
 
 public class Main {
     public static void main(String[] args) {
-        Document document = new Document(PageSize.A4, 36, 36, 150, 36); // Establecer márgenes
+        int x  =1;
+        for (int i = 0; i < x; i++) {
+            Document document = new Document(PageSize.A4, 36, 36, 150, 36); // Establecer márgenes
 
-        try {
-            FileOutputStream fos = new FileOutputStream("formato_silabo1.pdf");
-            PdfWriter writer = PdfWriter.getInstance(document, fos);
+            try {
+                FileOutputStream fos = new FileOutputStream(i+"_formato_silabo.pdf");
+                PdfWriter writer = PdfWriter.getInstance(document, fos);
 
-            // Establecer el evento del encabezado
-            EncabezadoDos encabezado = new EncabezadoDos();
-            writer.setPageEvent(encabezado);
+                // Establecer el evento del encabezado
+                EncabezadoDos encabezado = new EncabezadoDos();
+                writer.setPageEvent(encabezado);
 
-            document.open();
+                document.open();
 
                 facultadEscuela(document);
 
                 datosBasicosModulo(document);
 
-            document.close();
-            fos.close();
+                tableUnaColumna(document, "     II.                 DESCRIPCIÓN DEL MODULO", "La calidad y el uso de los medios de transmisión determinal en gran medida la calidad de la comunicación percibida por los usuarios, también presentan una gran influencia en los costos y capacidades de transmisión. Es el medio de transmisión el principal elemento a tener en cuenta en la generación de comunicaciones a nivel físico.\n" +                        "Este espacio brinda las herramientas necesarias para entender la aplicabilidad de los diferentes medios utilizados en el mercado de las comunicaciones, entendiendo los mecanismos que los gobiernan, junto con sus ventajas y desventajas, adicionalmente describe los efectos sobre el espacio debido a la presencia de cargas eléctricas y distribuciones variantes en el tiempo de carga eléctricas (electromagnetismo), al igual que las propiedades físicas emergentes de la dinámica de grupos de carga que definen propiedades eléctricas y magnéticas en un medio de transmisión guiado y son la base del funcionamiento de varios dispositivos como el UTP, FTP, STP, Par telefónico, coaxial, Guías de ondas entre otros\n");
 
-            System.out.println("PDF creado o modificado");
+                document.close();
+                fos.close();
 
-        } catch (FileNotFoundException | DocumentException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-            System.out.println("Error al crear o modificar el PDF");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+                System.out.println("PDF creado o modificado");
+
+            } catch (FileNotFoundException | DocumentException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                System.out.println("Error al crear o modificar el PDF");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
+
+
+
+
     }
     public static void facultadEscuela(Document document) throws DocumentException {
         //Fuente
@@ -76,46 +86,110 @@ public class Main {
         table.setTotalWidth(pageWidth * 0.8f); // Ancho de la tabla (en puntos)
         table.setLockedWidth(true); // Bloquear el ancho de la tabla
 
+        //Modificar tamaño celdas
         float[] columnWidths = {1, 2};
         table.setWidths(columnWidths);
 
 
-        //agregar celdas a la tabla
+        //agregar celda superior a la tabla
         PdfPCell cell1 =  new PdfPCell(new Phrase("     I.                  DATOS BÁSICOS DEL MODULO", FontFactory.getFont("Gill Sans MT", 8, Font.BOLD, BaseColor.WHITE)));
 
-        PdfPCell cell2 =  new PdfPCell(new Phrase("NOMBRE MODULO", FontFactory.getFont("Gill Sans MT", 8, Font.BOLD)));
-        PdfPCell cell3 =  new PdfPCell(new Phrase("Medios de Transmisión", FontFactory.getFont("Gill Sans MT", 9)));
+        //Unificar celdas superiores
+        cell1.setColspan(2);
+        BaseColor color = new BaseColor(16,52,92);
+        cell1.setBackgroundColor(color);
+        //Agregar celda uno a la tabla
+        table.addCell(cell1);
 
-        PdfPCell cell4 =  new PdfPCell(new Phrase("CÓDIGO DEL MODULO", FontFactory.getFont("Gill Sans MT", 8, Font.BOLD)));
-        PdfPCell cell5 =  new PdfPCell(new Phrase("TIC49999", FontFactory.getFont("Gill Sans MT", 9)));
+        PdfPCell[] contenido = creatorCells("Medios de Transmisión","TIC49999","Pregrado", "Ingeniería en Telecomunicaciones", "Programa", "4", "16 semanas");
+        for (int i = 0; i < contenido.length; i++) {
+            table.addCell(contenido[i]);
+        }
+        document.add(table);
+    }
+
+    public static PdfPCell[] creatorCells(String nombreModulo, String codigoModulo, String cicloFormacion, String programa, String bloqueFormacion, String numeroCreditos, String duracion){
 
 
 
-        // Configurar celdas
+        PdfPCell cell1 = cellA("NOMBRE MODULO");
+        PdfPCell cell2 = cellB(nombreModulo);
+
+        PdfPCell cell3 = cellA("CÓDIGO DEL MODULO");
+        PdfPCell cell4 = cellB(codigoModulo);
+
+        PdfPCell cell5 = cellA("CICLO DE FORMACIÓN");
+        PdfPCell cell6 = cellB(cicloFormacion);
+
+        PdfPCell cell7 = cellA("PROGRAMA");
+        PdfPCell cell8 = cellB(programa);
+
+        PdfPCell cell9 = cellA("BLOQUE DE FORMACIÓN");
+        PdfPCell cell10 = cellB(bloqueFormacion);
+
+        PdfPCell cell11 = cellA("NÚMERO DE CRÉDITOS");
+        PdfPCell cell12 = cellB(numeroCreditos);
+
+        PdfPCell cell13 = cellA("DURACIÓN");
+        PdfPCell cell14 = cellB(duracion);
+
         cell1.setHorizontalAlignment(Element.ALIGN_LEFT);
         cell2.setHorizontalAlignment(Element.ALIGN_LEFT);
         cell3.setHorizontalAlignment(Element.ALIGN_LEFT);
         cell4.setHorizontalAlignment(Element.ALIGN_LEFT);
         cell5.setHorizontalAlignment(Element.ALIGN_LEFT);
+        cell6.setHorizontalAlignment(Element.ALIGN_LEFT);
+        cell7.setHorizontalAlignment(Element.ALIGN_LEFT);
+        cell8.setHorizontalAlignment(Element.ALIGN_LEFT);
+        cell9.setHorizontalAlignment(Element.ALIGN_LEFT);
+        cell10.setHorizontalAlignment(Element.ALIGN_LEFT);
+        cell11.setHorizontalAlignment(Element.ALIGN_LEFT);
+        cell12.setHorizontalAlignment(Element.ALIGN_LEFT);
+        cell13.setHorizontalAlignment(Element.ALIGN_LEFT);
+        cell14.setHorizontalAlignment(Element.ALIGN_LEFT);
+
+        PdfPCell [] contenido = {cell1, cell2,cell3,cell4,cell5,cell6,cell7,cell8,cell9,cell10,cell11,cell12,cell13,cell14};
+
+        return contenido;
 
 
-        //Unificar celdas
-        cell1.setColspan(2);
-        BaseColor color = new BaseColor(16,52,92);
-        cell1.setBackgroundColor(color);
-
-        //
-        // Agregar celdas a la tabla
-        table.addCell(cell1);
-        table.addCell(cell2);
-        table.addCell(cell3);
-        table.addCell(cell4);
-        table.addCell(cell5);
-
-
-        document.add(table);
+    }
+    public static PdfPCell cellA(String con){
+        PdfPCell cella =  new PdfPCell(new Phrase(con, FontFactory.getFont("Gill Sans MT", 8, Font.BOLD)));
+        return cella;
+    }
+    public static PdfPCell cellB(String con){
+        PdfPCell cellb =    new PdfPCell(new Phrase(con, FontFactory.getFont("Gill Sans MT", 9)));
+        return cellb;
     }
 
+    public static PdfPTable tableUnaColumna(Document document, String titulo, String contenido) throws DocumentException {
+        document.add(new Paragraph("  "));document.add(new Paragraph("  "));
+
+        //Tamaño de la hoja
+        float pageWidth = document.getPageSize().getWidth();
+        float pageHeight = document.getPageSize().getHeight();
+        //creacion de la tabla de una columna
+        PdfPTable table = new PdfPTable(1);
+        table.setWidthPercentage(80); // Ancho de la tabla (en porcentaje del ancho de la página)
+        table.setTotalWidth(pageWidth * 0.8f); // Ancho de la tabla (en puntos)
+        table.setLockedWidth(true); // Bloquear el ancho de la tabla
+        //creacion celdas
+        PdfPCell cell1 =  new PdfPCell(new Phrase(titulo, FontFactory.getFont("Gill Sans MT", 8, Font.BOLD, BaseColor.WHITE)));
+        PdfPCell cell2 =  new PdfPCell(new Phrase(contenido, FontFactory.getFont("Gill Sans MT", 9)));
+        //cambiar el color de la celda uno
+        BaseColor color = new BaseColor(16,52,92);
+        cell1.setBackgroundColor(color);
+        //Añadir celdas a la tabla
+        table.addCell(cell1);
+        table.addCell(cell2);
+        //agragar la tabla al docu
+        document.add(table);
+        //retorna la tabla
+        return table;
+
+
+    }
 }
 
 
